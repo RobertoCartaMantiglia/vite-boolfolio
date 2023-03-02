798338a9dba290f93a26824cf0702d48f7504d11
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-03-02 17:42:08 +0100

    add bottstrap and  component

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+    name: 'Appmain',
+    components: '',
+}
+</script>
+
+<template lang="">
+    <div>
+        <h1>ciao</h1>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
